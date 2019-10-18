48f0668a6824d708eaf684f7ca968e62609f5c74
github username <github email address>
2019-10-18 15:21:14 -0400

    fixed db connection issue.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+connection_details = YAML::load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(connection_details)
