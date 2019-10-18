6d0c75628ac91c61c9ee2d99c4c18a45bd677af8
github username <github email address>
2019-10-18 14:37:44 -0400

    removed all files

--- db/migrate/005_create_characters.rb
+++
@@ -1,9 +0,0 @@
-class CreateCharacters < ActiveRecord::Migration[5.1]
-    def change
-      create_table :characters do |t|
-        t.string :name
-        t.string :actor_id
-        t.string :show_id
-      end
-    end
-  end
