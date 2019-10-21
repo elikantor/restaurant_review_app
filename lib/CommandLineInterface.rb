41a685a1d41612197e923265233ef1a0f845455a
github username <github email address>
2019-10-21 18:01:23 -0400

    stuff

--- lib/CommandLineInterface.rb
+++ lib/CommandLineInterface.rb
@@ -11,7 +11,7 @@ class CommandLineInterface
         
             if user_input == "Sign In"
                 puts "signed in"
-                signed_in
+                choose_user
             elsif user_input == "Sign Up"
                 puts "signed up"
                 sign_up
@@ -33,54 +33,65 @@ class CommandLineInterface
         signed_in
     end
 
+    def choose_user
+        prompt = TTY::Prompt.new
+        user_input = prompt.select ("Please select your username") do |menu|
+            User.all.each do |user|
+                menu.choice "#{user.name}"
+            end
+        end
+        @user_id = User.find_by(name: user_input).id
+        signed_in
+    end
+
     def signed_in
         prompt = TTY::Prompt.new
-        new_user_input = prompt.select ("What would you like to do") do |menu|
+        user_input = prompt.select ("What would you like to do") do |menu|
         menu.choice "Write a review"
-            write_review
         menu.choice "Get a restaurant recommendation for home"
         menu.choice "Get a restaurant recommendation for working/studying"
         menu.choice "Log out"
         end
         
-            if new_user_input == "Write a review"
+            if user_input == "Write a review"
                 write_review
-            elsif new_user_input == "Get a restaurant recoomendation for home"
+            elsif user_input == "Get a restaurant recoomendation for home"
                 sign_up
-            elsif new_user_input == "Get a restaurant recoomendation for working/studying"
+            elsif user_input == "Get a restaurant recoomendation for working/studying"
                 sign_up
-            elsif new_user_input == "Log out"
+            elsif user_input == "Log out"
                 home_screen
             end
     end
 
     def write_review
-        name_id = 
-        
+        name_id = @user_id
+
         puts "Please enter the name of a restaurant."
             res_name = gets.chomp.capitalize
             Restaurant.all.select do |restaurant|
                 if restaurant.name == res_name
-                    rest_id = restaurant.id 
-                else
-                    puts "This restaurant does not exist in our database."
-                    puts "What is the restaurants food genre?"
-                    food_genre = gets.chomp
-                    puts "What is the location of this restaurant?"
-                    location = gets.chomp
-                    puts "What is the average price rating of this restaurant"
-                    dollar_rating = gets.chomp
-
-                    Restaurant.create({name: res_name, food_genre: food_genre, location: location, :$rating => dollar_rating})
-                    rest_id = Restaurant.where("name = res_name").id
-
+                    rest_id = Restaurant.find_by(name: restaurant.name).id 
                 end
             end
-        puts "Please enter your review."
-            review = gets.chomp
-        puts "Please enter your rating between 1-5."
+            
+            if !Restaurant.exists?(name: res_name)
+                puts "This restaurant does not exist in our database."
+                puts "What is the restaurants food genre?"
+                food_genre = gets.chomp
+                puts "What is the location of this restaurant?"
+                location = gets.chomp
+                puts "What is the average price rating of this restaurant"
+                dollar_rating = gets.chomp
+                Restaurant.create({name: res_name, food_genre: food_genre, location: location, :$rating => dollar_rating})
+                rest_id = Restaurant.find_by(name: res_name).id
+            end
+        
+            puts "Please enter your review."
+                review = gets.chomp
+            puts "Please enter your rating between 1-5."
             # while rating != 1...5
-                 number_rating = gets.chomp
+                number_rating = gets.chomp
             #     if rating = 1...5
             #         rating = number_rating
             #     else puts "Please enter your rating between 1-5."
