4385759ce95d809a28df90470aad1911babba228
Ryan <rngao97@gmail.com>
2019-10-21 16:12:30 -0400

    files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-10.times do 
-    User.create(name: Faker::Name.first_name, favorite_food_genre: Faker::Restaurant.type, home_location: Faker::Address.community, work_study_location: Faker::Address.community)
-    Restaurant.create(name: Faker::Restaurant.name, food_genre: Faker::Restaurant.type, location: Faker::Address.community)
-    Review.create(user_id: User.all.sample.id, restaurant_id: Restaurant.all.sample.id, content: Faker::Hacker.say_something_smart)
-end
+# 10.times do 
+#     User.create(name: Faker::Name.first_name, favorite_food_genre: Faker::Restaurant.type, home_location: Faker::Address.community, work_study_location: Faker::Address.community)
+#     Restaurant.create(name: Faker::Restaurant.name, food_genre: Faker::Restaurant.type, location: Faker::Address.community)
+#     Review.create(user_id: User.all.sample.id, restaurant_id: Restaurant.all.sample.id, content: Faker::Hacker.say_something_smart)
+# end
