66fdb4a62ef08e1d89bc25716b199707ccd6a8a5
github username <github email address>
2019-10-22 11:54:21 -0400

    added new code

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -2,16 +2,5 @@ class Restaurant < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
 
-    def self.average_rating  
-        count = 0
-        agg_rating = 0 
-        Review.all.each do |review|
-            if review.restaurant == self
-                count += 1
-                agg_rating += self.rating 
-            end
-        end
-        agg_rating / count
-    end
 
 end
