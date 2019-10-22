b2799238d54f3de9365291e1d1fc7f2fc8f0b58f
github username <github email address>
2019-10-22 14:56:49 -0400

    moved methods to methods model

--- lib/CommandLineInterface.rb
+++ lib/CommandLineInterface.rb
@@ -2,7 +2,6 @@ class CommandLineInterface
     require 'pry'
     require_relative './methods.rb'
 
-    p average_rating ("Burger King")
     def home_screen
         prompt = TTY::Prompt.new
         user_input = prompt.select ("Welcome to Flatiron Yelp, the restaurant review app") do |menu|
@@ -27,7 +26,7 @@ class CommandLineInterface
                 menu.choice "#{user.name}"
             end
         end
-        p User.find_by(name: user_input)
+        User.find_by(name: user_input)
         @user = User.find_by(name: user_input)
         signed_in
     end
@@ -47,7 +46,7 @@ class CommandLineInterface
 
     def signed_in
         prompt = TTY::Prompt.new
-        user_input = prompt.select ("Welcome #{@user.name}. What would you like to do") do |menu|
+        user_input = prompt.select ("Hello #{@user.name}. What would you like to do") do |menu|
         menu.choice "Write a review"
         menu.choice "See restaurants that have been reviewed"
         menu.choice "Your recommended restaurants"
@@ -80,22 +79,6 @@ class CommandLineInterface
         new_neighborhood_work = gets.chomp
         user = User.find_by(name: @user.name)
         user.update(name: new_name, favorite_food_genre: new_food_genre, home_location: new_neighborhood_home, work_study_location: new_neighborhood_work)
-    end
-  
-    def restaurant_list
-        Restaurant.all.each do |restaurant|
-            rest_arr = []
-            restaurant_id = restaurant.id
-            rest_arr << "Restaurant: " + restaurant.name
-            rest_arr << "Neighborhood: " + restaurant.location
-            Review.all.each do |review|
-                if review.restaurant_id == restaurant.id
-                    rest_arr << "Rating: " + review.rating.to_s
-                end
-            end
-            print rest_arr.join(" - ")
-        end
-        print "\n"
         signed_in
     end
 
@@ -117,103 +100,6 @@ class CommandLineInterface
         elsif user_input == "Go back"
             signed_in
         end
-    end
-
-    def restaurant_rec_home
-        arr = []
-        print_arr = []
-        # Review.all.each do |review|
-        #     if review.rating >= 4
-        #         arr << review.restaurant_id
-        #     end
-        # end
-
-        Restaurant.all.each do |restaurant|
-            if average_rating(restaurant.name) >= 4
-                arr << restaurant.id
-            end
-        end
-
-        arr.each do |id|
-            if Restaurant.find(id).location == @user.home_location
-                print_arr << Restaurant.find(id).name
-            end
-        end
-        puts print_arr
-        signed_in
-    end
-
-    def restaurant_rec_work_study
-        arr = []
-        print_arr = []
-        Review.all.each do |review|
-            if review.rating >= 4
-                arr << review.restaurant_id
-            end
-        end
-
-        arr.each do |id|
-            if Restaurant.find(id).location == @user.work_study_location
-                print_arr << Restaurant.find(id).name
-            end
-        end
-        puts print_arr
-        signed_in
-    end
-
-    def restaurant_rec_food_genre
-        arr = []
-        print_arr = []
-        Review.all.each do |review|
-            if review.rating >= 4
-                arr << review.restaurant_id
-            end
-        end
-
-        arr.each do |id|
-            if Restaurant.find(id).food_genre == @user_id.favorite_food_genre
-                print_arr << Restaurant.find(id).name
-            end
-        end
-        puts print_arr
-        signed_in
-    end
-
-    def write_review
-        name_id = @user.id
-        rest_id = 0
-        puts "Please enter the name of a restaurant."
-            res_name = gets.chomp.capitalize
-            Restaurant.all.select do |restaurant|
-                if restaurant.name == res_name
-                    rest_id = Restaurant.find_by(name: restaurant.name).id 
-                end
-            end
-            
-            if !Restaurant.exists?(name: res_name)
-                puts "This restaurant does not exist in our database."
-                puts "What is the restaurant's food genre?"
-                food_genre = gets.chomp
-                puts "What is the location of this restaurant?"
-                location = gets.chomp
-                puts "What is the average price rating of this restaurant"
-                dollar_rating = gets.chomp
-                Restaurant.create({name: res_name, food_genre: food_genre, location: location, :$rating => dollar_rating})
-                rest_id = Restaurant.find_by(name: res_name).id
-            end
-        
-            puts "Please enter your review."
-                review = gets.chomp
-            puts "Please enter your rating between 1-5."
-            # while rating != 1...5
-                number_rating = gets.chomp
-            #     if rating = 1...5
-            #         rating = number_rating
-            #     else puts "Please enter your rating between 1-5."
-            #     end
-            # end
-        Review.create({user_id: name_id, restaurant_id: rest_id, content: review, rating: number_rating })
-        signed_in
     end
 
 end
